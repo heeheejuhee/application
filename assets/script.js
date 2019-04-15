a38148d051da565af8c89240bda07ebb7bc6edbe
Juhee Han <juhee5056@gmail.com>
2019-04-15 19:57:06 +1200

    RamenLab Customize Page

--- assets/script.js
+++ assets/script.js
@@ -15,21 +15,109 @@ $(function(){
     })
 
     //General
-    $(".btn-back").click(function(e){
+    $(".btn-back, .do-nothing").click(function(e){
         e.preventDefault();
     })
 
 
-    $('[data-url]').on('click',function(){
-
+    $('.menu').on('click',function(e){
         var url = $(this).data('url');
-        console.log(url);
-        window.location.href = url ;
-    })
+        // var defaultCart = {
+        //     main : "Tonkotsu Ramen",
+        //     quantity:1,
+        //     addon:[
+        //         {
+        //             name:"Shrimp",
+        //             price:0.4,
+        //             quantity:0,
+        //             image:'assets/shrimp.png'
+        //         },
+        //         {
+        //             name:"Mushroom",
+        //             price:0.4,
+        //             quantity:0,
+        //             image:'assets/shrimp.png'
+        //         },
+        //         {
+        //             name:"Seaweed",
+        //             price:0.4,
+        //             quantity:0,
+        //             image:'assets/shrimp.png'
+        //         },
+        //         {
+        //             name:"Whatever",
+        //             price:0.4,
+        //             quantity:0,
+        //             image:'assets/shrimp.png'
+        //         },
+        //     ]
+        // }
+        // localStorage.setItem("mycart",JSON.stringify(defaultCart));
 
-    $('.increase').click(function(){
-        $('.quantity').html(function(i,val){
-            return var*1+1
+
+        if(url){
+            window.location.href = url ;
+        }
+    });
+
+    if($("#Customize").length > 0){
+        // var cart = JSON.parse(localStorage.getItem("mycart"));
+        // var quantity = $('.yourorder-box .quantity , .price-controller .add-box span');
+        // var ingredients = $('.ingredients-wrapper');
+        // var addonHtml = "";
+
+        // for(var i =0; i < cart.addon.length; i++){
+        //     var name = cart.addon[i].name;
+        //     var image = cart.addon[i].image;
+        //     var price = cart.addon[i].price;
+        //     var q = cart.addon[i].quantity;
+
+        //     addonHtml += `
+        //         <div class="ingredients" data-item="${name}">
+        //             <img src="${image}" alt="">
+        //              <div class="ingredients-content">
+        //                 <h2>${name}</h2>
+        //                 <p>$${price}</p>
+        //                 <div class="add-box add-box-small">
+        //                     <button class="minus">-</button>
+        //                     <span>${q}</span>
+        //                     <button class="plus">+</button>
+        //                 </div>
+        //             </div>
+        //         </div>`;
+        // }
+
+        // quantity.text(cart.quantity);
+        // ingredients.append(addonHtml);
+
+        $('.add-box button').click(function(){
+            var addonName = $(this).parents('.ingredients').data('item');
+            var currentQ = parseInt($(this).siblings('span').text());
+            var isQuantity = $(this).parents('.add-box').hasClass('quantity-js');
+
+            if(this.className == "plus"){
+                $(this).siblings('span').text(currentQ + 1);
+                if(isQuantity){
+                    var q = $('.yourorder-box .quantity');
+                    q.text(parseInt(q.text())+1);
+                }
+            } else {
+                if(currentQ > 0 ){
+                    $(this).siblings('span').text(currentQ - 1);
+                    if(isQuantity){
+                        var q = $('.yourorder-box .quantity');
+                        q.text(parseInt(q.text())-1);
+                    }
+                }
+            }
+
         });
-    })
+    }
+
+
+    // $('.increase').click(function(){
+    //     $('.quantity').html(function(i,val){
+    //         return var*1+1
+    //     });
+    // })
 })
