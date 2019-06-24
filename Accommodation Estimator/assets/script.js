aa578105aed7ae9bb21bd2e0017528c0a106d111
Juhee Han <juhee5056@gmail.com>
2019-06-24 22:45:10 +1200

    Homepage update

--- Accommodation Estimator/assets/script.js
+++ Accommodation Estimator/assets/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+    
+    $('a').click(function(e){
+    	e.preventDefault()
+    });
+    
     setTimeout(function(){
         $('.circular-navigation').addClass('circular-nav-loaded');
     },1000);
+
+
 });
