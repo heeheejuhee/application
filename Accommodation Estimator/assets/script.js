b87ed85bd2f56907316f21f3c6f168301631b412
Juhee Han <200859841@cit3-501m-08.local>
2019-06-25 15:42:47 +1200

    Accommodation page added

--- Accommodation Estimator/assets/script.js
+++ Accommodation Estimator/assets/script.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     	e.preventDefault()
     });
     
+    // $('')
+
     setTimeout(function(){
         $('.circular-navigation').addClass('circular-nav-loaded');
     },1000);
