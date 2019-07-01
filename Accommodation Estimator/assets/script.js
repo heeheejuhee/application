b2d3cf963c749108a7dadf1938f9fec064420f64
Chris Kang Mac-mini <chris.sm.kang542@gmail.com>
2019-07-01 21:53:43 +1200

    Result page

--- Accommodation Estimator/assets/script.js
+++ Accommodation Estimator/assets/script.js
@@ -1,5 +1,5 @@
-
 $(function(){
+
 	setTimeout(function(){
         $('.circular-navigation').addClass('circular-nav-loaded');
     },300);
@@ -36,7 +36,8 @@ $(function(){
         numberText.text(currentNumber);
     });
 
-    $('.see-result-btn').on('click',function(){
+    //Accommodation Result button
+    $('.see-result-btn:not(.total-cost)').on('click',function(){
         var peopleValue = parseInt($('.number-controller-people span').text());
         var daysValue = parseInt($('.number-controller-days span').text());
         var allTypes = $('.accommodation-type');
@@ -61,6 +62,39 @@ $(function(){
             types.hotel.addClass('active');
         }
 
-    })
+        $('.accommodation-type .type.active').unbind('click').on('click',function(){
+            var type = $(this).data('type');
+            var cost = $(this).data('cost');
+            updateStatus(type, cost, peopleValue, daysValue);
 
+            $('.layer').removeClass('active');
+            $('.layer.layer-totalcost').addClass('active');
+        })
+    });
+
+    $('.total-cost').on('click',function(){
+        $('#Result').addClass('active');
+    })
 })
+
+function updateStatus(t, c, p, d){
+    var totalcostPage = $('.layer-totalcost');
+    var cost = d * c;
+    var perPerson = cost / p;
+    var hasBreakfast = $('input[name=breakfast]:checked').val();
+
+    totalcostPage.find('.title').text(t);
+    totalcostPage.find('.people').text(p);
+    totalcostPage.find('.days').text(d);
+    totalcostPage.find('.final-cost').text(numberToDollar(cost));
+    totalcostPage.find('.per-person').text(numberToDollar(perPerson));
+    if(hasBreakfast){
+        totalcostPage.find('.has-breakfast').text(numberToDollar(p * 7));
+    } else {
+        totalcostPage.find('.has-breakfast').text(numberToDollar(0));
+    }
+}
+
+function numberToDollar(num){
+    return "$" + parseFloat(Math.round(num * 100) / 100).toFixed(2);
+}
