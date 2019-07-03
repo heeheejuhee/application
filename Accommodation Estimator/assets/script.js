0700c84ae9ecc783e3e518b84f8c5408fc7beaca
Juhee Han <juhee5056@gmail.com>
2019-07-03 21:15:01 +1200

    Javascript bug fix

--- Accommodation Estimator/assets/script.js
+++ Accommodation Estimator/assets/script.js
@@ -24,6 +24,8 @@ $(function(){
     	$(sLayer).addClass('active');
     });
 
+    $('.update-type').on('click',updateTypes);
+
     //Increase or Decrease Number//
     $('.number-controller button').on('click',function(e){
         var value = $(this).attr('class');
@@ -49,38 +51,14 @@ $(function(){
     
 
     //Accommodation Result button
-    $('.see-result-btn:not(.total-cost)').on('click',function(){
-        peopleValue = parseInt($('.number-controller-people span').text());
-        daysValue = parseInt($('.number-controller-days span').text());
-        var allTypes = $('.accommodation-type');
-        var types = {
-            hostel: allTypes.children('.hostel'),
-            motel: allTypes.children('.motel'),
-            hotel: allTypes.children('.hotel'),
-            house: allTypes.children('.house'),
-        };
-        allTypes.children('.type').removeClass('active');
-
-        if( peopleValue < 2 && daysValue < 11 ){
-            types.hostel.addClass('active');
-        }
-        if( peopleValue < 5 && daysValue < 16 && daysValue > 1 ){
-            types.house.addClass('active');
-        }
-        if( peopleValue > 1 && peopleValue < 5 && daysValue < 11 && daysValue > 2 ){
-            types.motel.addClass('active');
-        }
-        if(  peopleValue < 3 && daysValue < 6 ){
-            types.hotel.addClass('active');
-        }
-  
-    });
+    // $('.see-result-btn:not(.total-cost)').on('click',function(){
+    $('.number-controller .minus, .number-controller .plus').on('click',updateTypes);
 
     //Next button
     $('.next').on('click',function(e){
         e.preventDefault();
         var sLayer = $(this).data('target');
-        $('.active').removeClass('active');
+        $('.layer').removeClass('active');
         $(sLayer).addClass('active');
     });
 
@@ -88,7 +66,7 @@ $(function(){
         type = $(this).data('type');
         dailyCost = $(this).data('cost');
         accomodatCost = dailyCost*daysValue;
-        // updateStatus();
+        updateStatus();
 
         $('.layer').removeClass('active');
         $('.layer.layer-totalcost').addClass('active');
@@ -99,12 +77,13 @@ $(function(){
          $('#Result').addClass('active');
         if($('[name=breakfast][value=true]:checked').length>0){
             mealCost = mealPrice * daysValue *peopleValue;
-        }
+            
+        } else{ mealCost = 0}
 
         total = accomodatCost + mealCost;
         updateStatus();
         // console.log(total);
-    })
+    });
 
 
 })
@@ -123,7 +102,7 @@ $('.grid').masonry({
 
 function updateStatus(){
 
-    var totalcostPage = $('.layer-totalcost');
+    var totalcostPage = $('.layer-totalcost, .layer-choice');
     accomodatCost = daysValue*dailyCost;
     var perPerson = total / peopleValue;
    
@@ -152,4 +131,29 @@ function numberToDollar(num){
     return "$" + parseFloat(Math.round(num * 100) / 100).toFixed(2);
 }
 
+function updateTypes(){
+    peopleValue = parseInt($('.number-controller-people span').text());
+    daysValue = parseInt($('.number-controller-days span').text());
+    var allTypes = $('.accommodation-type');
+    var types = {
+        hostel: allTypes.children('.hostel'),
+        motel: allTypes.children('.motel'),
+        hotel: allTypes.children('.hotel'),
+        house: allTypes.children('.house'),
+    };
+    allTypes.children('.type').removeClass('active');
 
+    if( peopleValue < 2 && daysValue < 11 ){
+        types.hostel.addClass('active');
+    }
+    if( peopleValue < 5 && daysValue < 16 && daysValue > 1 ){
+        types.house.addClass('active');
+    }
+    if( peopleValue > 1 && peopleValue < 5 && daysValue < 11 && daysValue > 2 ){
+        types.motel.addClass('active');
+    }
+    if(  peopleValue < 3 && daysValue < 6 ){
+        types.hotel.addClass('active');
+    }
+  
+}
